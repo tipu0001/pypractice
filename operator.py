e56b29677fcfaaa8b077dd50e76616913dea48ab
Naimul Tipu <mohammadnaimultipu@gmail.com>
2025-07-16 12:55:21 +0600

    Update operator.py

--- operator.py
+++ operator.py
@@ -1,20 +1,19 @@
-# p=5
-# q=10
-# sum=p+q
-# sub=p-q
-# mul=p*q
-# div=p/q
-# rem=p%q
-#
-# print("The sum is",sum)
-# print("The subtraction is",sub)
-# print("The multiplication is",mul)
-# print("The dision is",div)
-#
-# print("The result",p>q)
-# print("The result",p==q)
-# print("The result",p>=q)
-# print("The result",p<q)
+p=5
+q=10
+sum=p+q
+sub=p-q
+mul=p*q
+div=p/q
+rem=p%q
+print("The sum is",sum)
+print("The subtraction is",sub)
+print("The multiplication is",mul)
+print("The dision is",div)
+
+print("The result",p>q)
+print("The result",p==q)
+print("The result",p>=q)
+print("The result",p<q)
 
 
 a=True
