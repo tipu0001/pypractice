e938851d1ea241c5ba02cd1aaa484bd6e3d1f62e
Naimul Tipu <mohammadnaimultipu@gmail.com>
2025-07-16 12:49:39 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,23 @@
+name="tipu"
+age="26"
+
+
+print (name+ " is first boy")
+print("roll " +name +" number is 10")
+print("Main problem of tipu is That")
+print(name+ "age is "+age)
+
+x=10
+y=10.4
+z=True
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+
+
+
