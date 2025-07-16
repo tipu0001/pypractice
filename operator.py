334ec7c0a6b4a0ba5293ebe3f67c2751221dc290
Naimul Tipu <mohammadnaimultipu@gmail.com>
2025-07-16 12:51:19 +0600

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,26 @@
+# p=5
+# q=10
+# sum=p+q
+# sub=p-q
+# mul=p*q
+# div=p/q
+# rem=p%q
+#
+# print("The sum is",sum)
+# print("The subtraction is",sub)
+# print("The multiplication is",mul)
+# print("The dision is",div)
+#
+# print("The result",p>q)
+# print("The result",p==q)
+# print("The result",p>=q)
+# print("The result",p<q)
+
+
+a=True
+b=False
+print("the reuslt" ,not a or b)
+
+
+
+
